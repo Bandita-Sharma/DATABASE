d26bd4fc6c76680911fc71c95a7dbd63315e4620
Bandita-Sharma <32154531+Bandita-Sharma@users.noreply.github.com>
2018-09-08 17:59:41 +0530

    Update ass15.py

--- ass15.py
+++ ass15.py
@@ -1,26 +1,31 @@
 #Que1-->Write a python script to create a databse of students named Students.
 import pymongo
 client=pymongo.MongoClient()
-database=client['Studentdb']
+'''CREATES DATABASE'''
+database=client['Studentdb']  
 print("database created")
+'''CREATES TABLE'''
 collection=database['studenttbl']
 print("table created")
 print()
 
 #Que2-->Take students name and marks(between 0-100) as input from user 10 times using loops.
 print('Enter Detail Of Student')
+record=[]
 for i in range (10):
-    fn=input('enter the name:')
-    mrk=int(input('enter the marks:'))
+    record.append({"name":input(),"Marks":int(input())})
+print()
 
 #Que3-->Add these values in two columns named "Name" and "Marks" with the appropriate data type.
-for i in range(10):
-    collection.insert_one({'Name':fn,'Mark':mrk})
+'''insert rows in table'''
+data=collection.insert_many(record)   
 print('VALUES INSERTED IN TABLE')
+for data in collection.find():
+        print(data)
+print()
 
 #Que4-->Print the names of all the students who scored more than 80 marks.
-print('MARKS GREATER THAN 80')
-data=collection.find({'Mark':{"$gt" : 80}})
-for document in data:
-    print(document)
-
+print('MARKS GREATER THAN 80:')
+for document in collection.find():
+    if document["Marks"]>80:
+        print(document["name"])
